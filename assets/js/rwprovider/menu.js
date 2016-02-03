310e91f26b2be1eab7ea44f1373350b7474fa1ee
wesmangum <wesmangum24@gmail.com>
2016-02-03 09:44:14 -0600

    edit how the nav works

--- assets/js/rwprovider/menu.js
+++ assets/js/rwprovider/menu.js
@@ -1,10 +1,3 @@
-var slideout = new Slideout({
-    'panel': document.getElementById('panel'),
-    'menu': document.getElementById('menu'),
-    'padding': 226,
-    'tolerance': 70
-});
-
 $('.menu').on('click', function() {
-    slideout.toggle();
+    $('html').toggleClass('slideout-open');
 });
